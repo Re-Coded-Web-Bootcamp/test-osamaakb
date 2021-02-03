c12ee97fbd17cae62ced2c0349f34e7a80b42a97
osamaakb <osama98akb@gmail.com>
2021-02-03 17:48:56 +0300

    new good solution

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 function saturdayFun(activity = "roller-skate") {
     return `This Saturday, I want to ${activity}!`
 }
-
+let mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
 function wrapAdjective(visual = "*") {
     return function (however = "special") {
         return `You are ${visual}${however}${visual}!`
+
     }
 }
 wrapAdjective("%")("a dedicated programmer")
